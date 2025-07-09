6aebe8681b5fc65bc2e58138f5c5eea52331b13a
sagradev <mmpguty.5@gmail.com>
2025-07-08 21:07:49 -0400

    added a chance for random information

--- cypress/e2e/register.spec.cy.js
+++ cypress/e2e/register.spec.cy.js
@@ -2,7 +2,9 @@ import LoginPage from "../../Pages/loginPage"
 import userData from '../fixtures/user-data.json'
 import DashboardPage from "../../Pages/dashboardPage"
 import RegistratioPage from "../../Pages/registrationPage"
+const Chance = require('chance');
 
+const chance = new Chance();
 const loginPage = new LoginPage
 const dashboardPage = new DashboardPage
 const registrationPage = new RegistratioPage
