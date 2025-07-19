d6d069ccff41393984d216c364a45d452eeedee2
sagradev <mmpguty.5@gmail.com>
2025-07-18 23:26:59 -0400

    finalizes the transition case

--- Pages/bankInfo.js
+++ Pages/bankInfo.js
@@ -1,21 +1,36 @@
 class BankInfo {
-
-    selectorsList(){
-        const selectors = {
-            bankName:"[name='bankName']",
-            rountingNumber: "[name='routingNumber']",
-            accountNumber:"[name='accountNumber']"
-
-        }
-
-        return selectors
-
+    selectorsList() {
+        return {
+            bankName: "[name='bankName']",
+            routingNumber: "[name='routingNumber']", 
+            accountNumber: "[name='accountNumber']",
+            nextButton: "[data-test='user-onboarding-next']",
+            saveButton: "[data-test='bankaccount-submit']",
+            newTrasitionButton: "[href='/transaction/new']",
+            person: "[src='https://avatars.dicebear.com/api/human/uBmeaz5pX.svg']",
+            amount: "[name='amount']",
+            note: "[placeholder='Add a note']",
+            payment: "[data-test='transaction-create-submit-payment']"
+        };
     }
 
-    checkDashboardPage(){
-        cy.get(this.selectorsList().dashboardGrid).should('be.visible')
+    createBankAccount(name, routingNumber, accountNumber) {
+    cy.get(this.selectorsList().nextButton).click();
+    cy.get(this.selectorsList().bankName).type(name);
+    cy.get(this.selectorsList().routingNumber).type(routingNumber); 
+    cy.get(this.selectorsList().accountNumber).type(accountNumber);
+    cy.get(this.selectorsList().saveButton).click();
+    cy.get(this.selectorsList().nextButton).click();
     }
 
+    trasition(money, note){
+        cy.get(this.selectorsList().newTrasitionButton).click();
+        cy.get(this.selectorsList().person).click();
+        cy.get(this.selectorsList().amount).type(money);
+        cy.get(this.selectorsList().note).type(note);
+        cy.get(this.selectorsList().payment).click();
+        cy.get('body').should('contain', 'Transaction Submitted!')
+    }
 }
 
-export default BankInfo
+export default BankInfo;
--- cypress/e2e/transition.spec.cy.js
+++ cypress/e2e/transition.spec.cy.js
@@ -1,13 +1,25 @@
+import BankInfo from "../../Pages/bankInfo";
+import LoginPage from "../../Pages/loginPage"
+import userData from '../fixtures/user-data.json'
+const Chance = require('chance');
 
+const chance = new Chance();
+const bankInfo = new BankInfo
+const loginPage = new LoginPage
 
-describe('Enviar dinheiro com saldo suficiente', () => {
-  it('Deve enviar dinheiro com sucesso', () => {
-    // Implemente os passos do caso de teste aqui
+describe('Send money with sufficient balance', () => {
+  it('should send money successfully', () => {
+    loginPage.accessLoginPage()
+    loginPage.loginCase(userData.userSuccess.username, userData.userSuccess.password)
+    bankInfo.createBankAccount('teste','123456789','123456789')
+    bankInfo.trasition('0','teste')
   });
 });
 
 
-  it('Deve exibir mensagem de erro ao enviar dinheiro sem saldo suficiente', () => {
-    // Implemente os passos do caso de teste aqui
-  });
+it('error sending money without sufficient balance', () => {
+    loginPage.accessLoginPage()
+    loginPage.loginCase(userData.userSuccess.username, userData.userSuccess.password)
+    bankInfo.createBankAccount('teste','123456789','123456789')
+    bankInfo.trasition('10000','teste')
 });
